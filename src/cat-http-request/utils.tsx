38b106e69a45369543241908bf087bdb8fe7482a
Fábio Mateus Melo <fabioceto@gmail.com>
2023-07-03 12:22:37 -0300

    0001

--- src/cat-http-request/utils.tsx
+++ src/cat-http-request/utils.tsx
@@ -32,13 +32,7 @@ const HttpCatButton = () => {
           </>
         ) : null}
         <button onClick={handleClick}>Get Random HTTP Status</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 };
