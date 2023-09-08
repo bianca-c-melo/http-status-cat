9589b893af55150790dd063199af9d68f3fca824
Bianca Melo <81665351+bianca-c-melo@users.noreply.github.com>
2023-09-08 20:44:46 +0000

    add analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 // https://vitejs.dev/config/
 export default defineConfig({
